b0b8ceb64b04010e453584a523025487e5a803ff
Govind <govindj2001@gmail.com>
2025-03-09 18:48:21 +0530

    fixed mongoDB URI

--- server/database/db.js
+++ server/database/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const Connection= async ()=>{
     try {
-        const URL= `mongodb://${username}:${password}@blog-app-shard-00-00.vq2jg.mongodb.net:27017,blog-app-shard-00-01.vq2jg.mongodb.net:27017,blog-app-shard-00-02.vq2jg.mongodb.net:27017/?ssl=true&replicaSet=atlas-xv1aw6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Blog-app`;
+        const URL= process.env.MongoDB_URI;
         await mongoose.connect(URL);
         console.log('Database connected successfully');
     } catch(error) {
